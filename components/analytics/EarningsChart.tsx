48e1aa48a15e612db9e4b297e7c62681dd7cdf3d
skyegle0602 <tasubasa0602@gmail.com>
2026-01-22 21:51:38 +0100

    Add missing home and item

--- components/analytics/EarningsChart.tsx
+++ components/analytics/EarningsChart.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, BarChart, Bar } from 'recharts';
